42b022670dff85b210bc0377c16b8442084baa75
dohyunkim <dohyun.kim@bos-semi.com>
2025-01-22 16:48:32 +0900

    mod: Refactor utmd to multi-threading process

--- utmd.py
+++ utmd.py
@@ -1,52 +1,68 @@
+# ENV SERVER_IP, KAFKA_PORT, UTM_PORT, TOPIC_NAME
 import os
 import subprocess
 import sys
 import json
 import signal
 import logging
+import threading
+
+import requests
 from kafka import KafkaConsumer
 from dotenv import dotenv_values
+from payload import Payload
 
-PID_FILE = "/tmp/utmd.pid"
-LOG_FILE = "/tmp/utmd.log"
 TOPIC_NAME = None
-KAFKA_SERVER = None
-FOREGROUND = False
+SERVER_IP = None
+KAFKA_PORT = None
+UTM_PORT = None
+HOME_DIR = None
+PACKAGE_DIR = None
+PID_FILE = None
+srun_task_dict = {}
+logger = None
 
 def initialize():
-    global TOPIC_NAME, KAFKA_SERVER, FOREGROUND
+    global TOPIC_NAME, SERVER_IP, KAFKA_PORT, UTM_PORT, HOME_DIR, PACKAGE_DIR, PID_FILE, logger \
 
     TOPIC_NAME = os.getenv("TOPIC_NAME", "default")
-    KAFKA_SERVER = os.getenv("KAFKA_SERVER", "default")
-    FOREGROUND= os.getenv("FOREGROUND", "false").lower() == "true"
+    SERVER_IP = os.getenv("SERVER_IP", "default")
+    KAFKA_PORT = os.getenv("KAFKA_PORT", "9092")
+    UTM_PORT = os.getenv("UTM_PORT", "8023")
+    HOME_DIR = os.getenv("HOME")
+    PACKAGE_DIR = HOME_DIR + "/utmd"
+    PID_FILE = PACKAGE_DIR + "/tmp/utmd.pid"
+    log_dir = os.path.join(PACKAGE_DIR, "log")
+    os.makedirs(log_dir, exist_ok=True)
 
-    logging.info(f"Foreground mode: {FOREGROUND}")
+    # Setup logger
+    log_file = os.path.join(log_dir, "utmd.log")
+    logger= logging.getLogger()
+    logger.setLevel(logging.INFO)
+    # Avoid adding multiple handlers
+    if not logger.hasHandlers():
+        formatter = logging.Formatter(
+            "[%(asctime)s|%(levelname)-7s][%(name)s %(lineno)d %(funcName)s] %(message)s ",
+            datefmt='%Y-%m-%d %H:%M:%S')
 
-    # Logging config
-    logging.basicConfig(
-        filename=LOG_FILE,
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-    )
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        stream_handler.setLevel(logging.INFO)
+        logger.addHandler(stream_handler)
 
-def daemonize():
-    if os.fork() > 0:
-        sys.exit(0)
-    os.setsid()
-    if os.fork() > 0:
-        sys.exit(0)
-    sys.stdout.flush()
-    sys.stderr.flush()
-    with open("/dev/null", "wb", 0) as dev_null:
-        os.dup2(dev_null.fileno(), sys.stdin.fileno())
-        os.dup2(dev_null.fileno(), sys.stdout.fileno())
-        os.dup2(dev_null.fileno(), sys.stderr.fileno())
+        file_handler = logging.FileHandler(log_file, mode="a")
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.DEBUG)
+        logger.addHandler(file_handler)
+        print(f"Logger initialized. Log file at: {log_file}")
+
 
 def write_pid_file():
     if os.path.exists(PID_FILE):
-        logging.error("Daemon is already running.")
+        logger.error("Daemon is already running.")
         sys.exit(1)
-    with open(PID_FILE, "w") as f:
+    os.makedirs(os.path.dirname(PID_FILE), exist_ok=True)
+    with open(PID_FILE, "a") as f:
         f.write(str(os.getpid()))
 
 def remove_pid_file():
@@ -54,90 +70,131 @@ def remove_pid_file():
         os.remove(PID_FILE)
 
 def signal_handler(signum, frame):
-    logging.info(f"Received signal {signum}. Shutting down.")
+    logger.info(f"Received signal {signum}. Shutting down.")
     remove_pid_file()
     sys.exit(0)
 
-def process_message(message):
-    global FOREGROUND
+def send_complete_request(task_id, user):
+    url = f"http://{SERVER_IP}:{UTM_PORT}/api/task/complete"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "task_id": task_id,
+        "user": user
+    }
+    try:
+        response = requests.post(url, json=data, headers=headers)
+        if response.status_code >= 200 and response.status_code < 300:
+            logger.info(f"Complete request successfully send: {response.json()}")
+        else:
+            logger.error(f"Complete request failed: {response.status_code}, {response.text}")
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error occurred in send complete request: {e}")
+
+def execute_srun(payload):
+    try:
+        # Change Directory
+        os.chdir(payload.directory)
+        logger.info(f"Executing command: `{payload.command}` in directory: {payload.directory}")
+
+        srun_log_file = os.path.join(payload.directory, f"{payload.uuid}.log")
+        with open(srun_log_file, "a") as log_file:
+            log_file.write(f"Starting command: {payload.command}\n")
+
+            srun_process = subprocess.Popen(
+                payload.command,
+                shell=True,
+                stdout=log_file,
+                stderr=log_file,
+                stdin=subprocess.DEVNULL,
+                text=True,
+                env=payload.env,
+                start_new_session=True
+            )
+            logger.info(f"Started background process with PID: {srun_process.pid}")
+            log_file.write(f"Process started with PID: {srun_process.pid}\n")
+            srun_task_dict[payload.task_id] = srun_process
+
+            if srun_process.wait() == 0:
+                logger.info(f"Process with PID {srun_process.pid} completed successfully.")
+                # Send HTTP complete request
+                send_complete_request(payload.task_id, payload.user)
+            else:
+                logger.error(f"Process with PID {srun_process.pid} terminated with errors.")
+            return
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+
+def terminate_task(task_id):
+    process = srun_task_dict.get(task_id)
+    if process:
+        logger.info(f"Terminating task with ID: {task_id}")
+        process.terminate()
+        srun_task_dict.pop(task_id, None)
+    else:
+        logger.warning(f"No running task found with ID: {task_id}")
+
+def kill_task(task_id):
+    process = srun_task_dict.get(task_id)
+    if process:
+        logger.info(f"Killing task with ID: {task_id}")
+        process.kill()
+        srun_task_dict.pop(task_id, None)
+    else:
+        logger.warning(f"No running task found with ID: {task_id}")
+
+def validate_message(message):
     try:
         data = json.loads(message)
+        task_id = data.get("id")
+        user = data.get("user")
         directory = data.get("directory")
         uuid = data.get("uuid")
         command = data.get("command")
+        action = data.get("action")
+    except json.JSONDecodeError as e:
+        logger.error(f"Failed to parse message: {message}, Error: {e}")
 
-        if directory and uuid and command:
-            logging.info(f"Processing message: directory={directory}, uuid={uuid}, command={command}")
-
-            # Change Directory
-            os.chdir(directory)
+    if action and task_id and directory and uuid and command:
+        if action == "cancel" :
+            logger.info(f"Terminating task_id: {task_id}, command: {command}")
+            terminate_task(task_id)
+            return 0
+        elif action == "add" :
+            logger.info(f"Validating message: task_id={task_id}, directory={directory}, uuid={uuid}, command={command}")
 
             # Read env file
             env_path = f"{directory}/{uuid}.env"
-            srun_log_file = f"{directory}/{uuid}.log"
             if os.path.exists(env_path):
-                env_vars = dotenv_values(env_path)
-            else:
-                logging.error(f".env file not found at: {env_path}")
-                sys.exit(0)
-
-            logging.info(f"Executing command: {command}")
-
-            if FOREGROUND:
-                srun_process = subprocess.Popen(
-                    command,
-                    shell=True,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    text=True,
-                    env=env_vars
-                )
-                for line in iter(srun_process.stdout.readline, ""):
-                    print(line, end="")
-                for line in iter(srun_process.stderr.readline, ""):
-                    print(line, end="", file=sys.stderr)
-
-                exit_code = srun_process.wait()
-                print(f"srun completed with exit code: {exit_code}")
-                return exit_code
-            else:
-                with open(srun_log_file, "a") as log_file:
-                    log_file.write(f"Starting command: {command}\n")
-
-                    srun_process = subprocess.Popen(
-                        command,
-                        shell=True,
-                        stdout=log_file,
-                        stderr=log_file,
-                        text=True,
-                        env=env_vars,
-                        start_new_session=True
-                    )
-                    logging.info(f"Started background process with PID: {srun_process.pid}")
-                    log_file.write(f"Process started with PID: {srun_process.pid}\n")
-                    return srun_process.pid
-
-        else:
-            logging.warning(f"Missing required fields in message: {message}")
-    except json.JSONDecodeError as e:
-        logging.error(f"Failed to parse message: {message}, Error: {e}")
-    except Exception as e:
-        logging.error(f"Unexpected error: {e}")
+                env = dotenv_values(env_path)
+            else :
+                logger.error(f".env file not found at: {env_path}")
+                return 1
+            return Payload(task_id, user, command, uuid, directory, env)
+        else :
+            raise Exception("Parsing Exception in action")
+    else:
+        logger.error("Message validation failed. Unknown action/id/directory/uuid/command")
+        return 1
 
 def kafka_consumer():
-    logging.info("Starting Kafka consumer...")
+    logger.info("Starting Kafka consumer...")
     consumer = KafkaConsumer(
         TOPIC_NAME,
-        bootstrap_servers=[KAFKA_SERVER],
+        bootstrap_servers=[f"{SERVER_IP}:{KAFKA_PORT}"],
         auto_offset_reset="earliest",
         enable_auto_commit=True,
         group_id="my-group",
         value_deserializer=lambda x: x.decode("utf-8"),
     )
-
     for message in consumer:
-        logging.info(f"Received message: {message.value}")
-        process_message(message.value)
+        logger.info(f"Received message: {message.value}")
+        payload = validate_message(message.value)
+        if isinstance(payload, Payload):
+            thread = threading.Thread(target=execute_srun, args=(payload,))
+            thread.start()
+        else:
+            if not payload == 0:
+                logger.error("Error occurred in validate_message")
 
 def main():
     signal.signal(signal.SIGTERM, signal_handler)
@@ -146,16 +203,11 @@ def main():
     initialize()
 
     write_pid_file()
-
     try:
         kafka_consumer()
     finally:
         remove_pid_file()
+
 if __name__ == "__main__":
-    if os.getenv("FOREGROUND", "false").lower() == "true":
-        logging.info("Running in foreground mode.")
-        main()
-    else:
-        daemonize()
-        logging.info("Running in background mode.")
-        main()
+    logging.info("Running in background mode.")
+    main()
